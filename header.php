17197c6f159b7aa6c199d6b86471d21ab07b5efb
Mochamad Arifin <klampok.child@gmail.com>
2021-10-30 00:56:08 +0700

    Enhance layout

--- header.php
+++ header.php
@@ -25,18 +25,18 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'desa' ); ?></a>
 
-	<header id="masthead">
-		<div class="container">
+	<header id="masthead" class="mb-5">
+		<div class="container pt-3">
 			<div class="site-branding">
 				<?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
 					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h1 class="site-title h1"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php
 				else :
 					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<p class="site-title h2"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 					<?php
 				endif;
 				$desa_description = get_bloginfo( 'description', 'display' );
